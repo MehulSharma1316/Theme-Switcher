0e0fa6b1e6ae46420d0a283d147c5c01c7b75aa7
MehulSharma1316 <mehulsharma1316@gmail.com>
2023-12-20 21:08:28 +0530

    first commit Theme Switcher

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {createContext, useContext} from 'react'
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    lightTheme: () => {},
+    darkTheme: () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme () {
+    return useContext(ThemeContext)
+}
